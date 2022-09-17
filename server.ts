ad62c049a7e64faea978bf005b0c7fe2e52c3dba
Melissa Rose <123melissa321@gmail.com>
2022-09-17 22:28:00 +0100

    possibletimes route returns possible booking times for date/numberofpeople

--- server.ts
+++ server.ts
@@ -15,7 +15,7 @@ const herokuSSLSetting = { rejectUnauthorized: false }
 const sslSetting = process.env.LOCAL ? false : herokuSSLSetting
 const dbConfig = {
   connectionString: process.env.DATABASE_URL,
-  ssl: sslSetting,
+  ssl: herokuSSLSetting,
 };
 
 const app = express();
@@ -27,9 +27,38 @@ const client = new Client(dbConfig);
 client.connect();
 
 app.get("/", async (req, res) => {
-  const dbres = await client.query('select * from categories');
+  const dbres = await client.query('select * from tables');
   res.json(dbres.rows);
 });
+
+app.get("/possibletimes/:date/:numberofpeople", async (req, res) => {
+  try {
+    const date: string = req.params.date;
+    const numberOfPeople: number = parseInt(req.params.numberofpeople);
+    const possibleTables = await client.query('select table_id from tables where capacity >= $1', [numberOfPeople])
+    const unavailableTimes = await client.query('select bookings.table_id, bookings.time from bookings left join tables on bookings.table_id = tables.table_id where date=$1 and bookings.table_id in (select table_id from tables where capacity >= $2)', [date, numberOfPeople])
+    let availableTimes: string[] = [];
+    for (let table_id of possibleTables.rows) {
+      let times=['17:00', '17:30', '18:00', '18:30', '19:00', '19:30', '20:00', '20:30', '21:00', '21:30', '22:00'];
+      for (let row of unavailableTimes.rows) {
+        if ((row.table_id === table_id) && (times.includes(row.time))) {
+          let index = times.indexOf(row.time);
+          times = times.splice(index, 3); //remove any unavailble times from the array of possible times for this table_id
+        }
+      }
+      availableTimes = [...availableTimes, ...times];
+    }
+    let uniqueAvailableTimes: string[] = [];
+    for (let time of availableTimes) {
+      if (!uniqueAvailableTimes.includes(time)) {
+        uniqueAvailableTimes.push(time);
+      }
+    }
+    res.send(uniqueAvailableTimes);
+  } catch (error) {
+    console.error(error);
+  }
+})
 
 
 //Start the server on the given port
