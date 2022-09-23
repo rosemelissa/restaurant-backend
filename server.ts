4233350b689d7f65f0b80961665a033d67eda6b6
Melissa Rose <123melissa321@gmail.com>
2022-09-23 13:02:37 +0100

    Removed comments and console.logs

--- server.ts
+++ server.ts
@@ -47,20 +47,6 @@ app.get("/possibletimes/:date/:numberofpeople", async (req, res) => {
         availableTimes.push(time)
       }
     }
-    
-    
-    
-    // let availableTimes: string[] = [];
-    // for (let table_id of possibleTables.rows) {
-    //   let times=['17:00', '17:30', '18:00', '18:30', '19:00', '19:30', '20:00', '20:30', '21:00', '21:30', '22:00'];
-    //   for (let row of unavailableTimes.rows) {
-    //     if ((row.table_id === table_id) && (times.includes(row.time))) {
-    //       let index = times.indexOf(row.time);
-    //       times = times.splice(index, 3); //remove any unavailble times from the array of possible times for this table_id
-    //     }
-    //   }
-    //   availableTimes = [...availableTimes, ...times];
-    // }
     let uniqueAvailableTimes: string[] = [];
     for (let time of availableTimes) {
       if (!uniqueAvailableTimes.includes(time)) {
@@ -84,26 +70,17 @@ app.post("/newbooking", async (req, res) =>{
       time,} = req.body;
     const customerDb = await client.query('insert into customers(firstname, surname, email, mailing_list) values ($1, $2, $3, $4) returning customer_id', [firstname, surname, email, mailingList])
     const {customer_id} = customerDb.rows[0]
-    console.log('customer id', customer_id)
     const possibleTablesDb = await client.query('select table_id from tables where capacity>=$1 order by capacity asc', [numberOfPeople]);
     const possibleTables = possibleTablesDb.rows
-    console.log('possible table', possibleTables)
     let chosenTableId;
-    // const allTimes=['17:00', '17:30', '18:00', '18:30', '19:00', '19:30', '20:00', '20:30', '21:00', '21:30', '22:00'];
-    // const index = allTimes.indexOf(time);
-    // console.log(index);
-    // const timesForThisBooking = [allTimes[index], allTimes[index + 1], allTimes[index + 2]]
-    // console.log(timesForThisBooking)
     for (let row of possibleTables) {
       const {table_id} = row
       const bookingsDb = await client.query('select time from bookings where table_id=$1 and date=$2', [table_id, date])
       const bookingsForThatTable = bookingsDb.rows;
       if (tableIsAvailable(bookingsForThatTable, time)) {
         chosenTableId = table_id;
-        console.log('using table', chosenTableId)
         break;
       }
-      console.log('bookingsforthattable', bookingsForThatTable)
     }
     if (chosenTableId) {
       const booking_id = await client.query('insert into bookings(customer_id, table_id, date, time, covers) values ($1 ,$2, $3, $4, $5) returning booking_id', [customer_id, chosenTableId, date, time, numberOfPeople])
@@ -123,8 +100,6 @@ app.get("/covers/:date", async (req, res) => {
     console.log(sevenDays)
     console.log(sevenDays)
     const coversDb = await client.query('select date, sum(covers) as total_covers from bookings where date in ($1, $2, $3, $4, $5, $6, $7) group by date', [...sevenDays])
-
-    // const coversDb = await client.query('select date, sum(covers) as total_covers from bookings where date in $1 group by date', [sevenDays])
     res.json(coversDb.rows)
   } catch (error) {
     console.error(error);
